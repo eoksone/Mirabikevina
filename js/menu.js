3d104e80eedec10a1f6ca9c0240a4ceedb649dee
Rodrigo <rcifuentes.osorio@gmail.com>
2023-09-30 00:28:32 -0300

    primer commit

---
+++ js/menu.js
@@ -0,0 +1,24 @@
+let listElements = document.querySelectorAll('.list_boton--click');
+
+
+listElements.forEach(listElements => {
+    listElements.addEventListener('click',()=>{
+
+        listElements.classList.toggle('arrow');
+
+          let height = 0;
+          let menu = listElements.nextElementSibling;
+          if(menu.clientHeight == "0"){
+            height=menu.scrollHeight;
+          }
+
+          menu.style.height =  `${height}px`;
+
+
+
+
+
+    })
+ 
+
+});
